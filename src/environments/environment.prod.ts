b655368011cd12fd18678fec3e143f0cccd885d7
christopherblackza <christopher.black.sa@gmail.com>
2026-02-13 22:57:38 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
     url: 'https://kwlrkrjraajnkrlgcjao.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3bHJrcmpyYWFqbmtybGdjamFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMzUyNDEsImV4cCI6MjA2NzkxMTI0MX0.NWlxRMHjlhXg3ueYx5cnBu_SAm4qg7VFrBCBxNzHCi0' // Replace with actual anon key
   },
-  apiUrl: 'https://kwlrkrjraajnkrlgcjao.supabase.co/rest/v1'
+  apiUrl: 'https://kwlrkrjraajnkrlgcjao.supabase.co/rest/v1',
+  nestApiUrl: 'https://meetnestmcp-production.up.railway.app'
 };
